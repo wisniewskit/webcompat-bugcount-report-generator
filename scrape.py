caff212c5359228a281a68917571c7611c2673e1
John Giannelos <jgiannelos@mozilla.com>
2019-01-17 18:36:36 +0100

    Add scraping script for webcompat metrics

---
+++ scrape.py
@@ -0,0 +1,92 @@
+import os
+import requests
+
+from datetime import datetime
+from requests_html import HTMLSession
+from tablib import Dataset
+
+
+DATA_PATH = os.path.join(os.getcwd(), 'data/top-sites-report-card.xslx')
+EXPORT_PATH = os.path.join(os.getcwd(), 'data/export/{}.csv'.format(datetime.now().strftime('%Y%m%d-%H%M%S')))
+GITHUB_API_TOKEN = os.environ.get('GITHUB_API_TOKEN')
+
+
+def get_websites(dataset):
+    websites = dataset.get_col(1)
+    return websites
+
+def get_col_c(website):
+    template = 'https://bugzilla.mozilla.org/buglist.cgi?bug_file_loc_type=allwordssubstr&list_id=14485116&resolution=---&bug_file_loc={}&query_format=advanced&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&product=Core&product=Firefox&product=Firefox%20for%20Android&product=Tech%20Evangelism'
+    query = template.format(website)
+    session = HTMLSession()
+    r = session.get(query)
+    count_el = r.html.find('span.bz_result_count', first=True)
+    count = int(count_el.text.rstrip('bugs found.'))
+    return '=HYPERLINK({}, {})'.format(query, count)
+
+def get_col_d(website):
+    template = 'https://github.com/search?p=8&q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+state%3Aopen&type=Issues'
+    query = template.format(website)
+    search_template = 'https://api.github.com/search/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+state%3Aopen&type=Issues'
+    search = search_template.format(website)
+    response = requests.get(search, headers = {'Authorization': 'token {}'.format(GITHUB_API_TOKEN)}).json()
+    count = response['total_count']
+    return '=HYPERLINK({}, {})'.format(query, count)
+
+def get_col_e(website):
+    template = 'https://github.com/webcompat/web-bugs/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+label%3Aseverity-critical+'
+    query = template.format(website)
+    search_template = 'https://api.github.com/search/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+label%3Aseverity-critical+'
+    search = search_template.format(website)
+    response = requests.get(search, headers = {'Authorization': 'token {}'.format(GITHUB_API_TOKEN)}).json()
+    count = response['total_count']
+    return '=HYPERLINK({}, {})'.format(query, count)
+
+def get_col_f(website):
+    template = 'https://github.com/search?p=8&q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+milestone%3Aneedsdiagnosis'
+    query = template.format(website)
+    search_template = 'https://api.github.com/search/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+milestone%3Aneedsdiagnosis'
+    search = search_template.format(website)
+    response = requests.get(search, headers = {'Authorization': 'token {}'.format(GITHUB_API_TOKEN)}).json()
+    count = response['total_count']
+    return '=HYPERLINK({}, {})'.format(query, count)
+
+def get_col_g(website):
+    template = 'https://github.com/webcompat/web-bugs/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+milestone%3Asitewait'
+    query = template.format(website)
+    search_template = 'https://api.github.com/search/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+milestone%3Asitewait'
+    search = search_template.format(website)
+    response = requests.get(search, headers = {'Authorization': 'token {}'.format(GITHUB_API_TOKEN)}).json()
+    count = response['total_count']
+    return '=HYPERLINK({}, {})'.format(query, count)
+
+
+if __name__ == '__main__':
+    dataset_in = Dataset()
+    dataset_in.load(open(DATA_PATH, 'rb').read())
+
+    dataset_out = Dataset(headers=[
+        'Website',
+        'Bugzilla',
+        'Github - open',
+        'Github - severity-critical',
+        'Github - needsdiagnosis',
+        'Github - sitewait'
+    ])
+
+    websites = get_websites(dataset_in)
+    for website in websites:
+        row = [
+            website,
+            get_col_c(website),
+            get_col_d(website),
+            get_col_e(website),
+            get_col_f(website),
+            get_col_g(website),
+        ]
+
+        print(row)
+        dataset_out.append(row)
+
+    with open(EXPORT_PATH, 'w') as f:
+        f.write(dataset_out.csv)
