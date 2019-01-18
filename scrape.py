2bccd0033c88118ac1e0f4990de3eb52617a26a1
John Giannelos <jgiannelos@mozilla.com>
2019-01-18 17:00:33 +0100

    Fix HYPERLINK syntax. Retry GH API requests on rate limit.

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import time
 
 from datetime import datetime
 from requests_html import HTMLSession
@@ -15,50 +16,64 @@ def get_websites(dataset):
     websites = dataset.get_col(1)
     return websites
 
+def api_request(*args, **kwargs):
+    try:
+        response = requests.get(*args, **kwargs)
+        response.raise_for_status()
+        print(response)
+        return response
+    except requests.exceptions.HTTPError:
+        print('Sleeping...')
+        time.sleep(60)
+        return api_request(*args, **kwargs)
+
 def get_col_c(website):
     template = 'https://bugzilla.mozilla.org/buglist.cgi?bug_file_loc_type=allwordssubstr&list_id=14485116&resolution=---&bug_file_loc={}&query_format=advanced&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&product=Core&product=Firefox&product=Firefox%20for%20Android&product=Tech%20Evangelism'
     query = template.format(website)
     session = HTMLSession()
     r = session.get(query)
-    count_el = r.html.find('span.bz_result_count', first=True)
-    count = int(count_el.text.rstrip('bugs found.'))
-    return '=HYPERLINK({}, {})'.format(query, count)
+    try:
+        count_el = r.html.find('span.bz_result_count', first=True)
+        count = int(count_el.text.rstrip('bugs found.'))
+    except:
+        count = 0
+    return '=HYPERLINK("{}"; {})'.format(query, count)
 
 def get_col_d(website):
     template = 'https://github.com/search?p=8&q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+state%3Aopen&type=Issues'
     query = template.format(website)
     search_template = 'https://api.github.com/search/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+state%3Aopen&type=Issues'
     search = search_template.format(website)
-    response = requests.get(search, headers = {'Authorization': 'token {}'.format(GITHUB_API_TOKEN)}).json()
+    response = api_request(search, headers = {'Authorization': 'token {}'.format(GITHUB_API_TOKEN)}).json()
     count = response['total_count']
-    return '=HYPERLINK({}, {})'.format(query, count)
+    return '=HYPERLINK("{}"; {})'.format(query, count)
 
 def get_col_e(website):
     template = 'https://github.com/webcompat/web-bugs/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+label%3Aseverity-critical+'
     query = template.format(website)
     search_template = 'https://api.github.com/search/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+label%3Aseverity-critical+'
     search = search_template.format(website)
-    response = requests.get(search, headers = {'Authorization': 'token {}'.format(GITHUB_API_TOKEN)}).json()
+    response = api_request(search, headers = {'Authorization': 'token {}'.format(GITHUB_API_TOKEN)}).json()
     count = response['total_count']
-    return '=HYPERLINK({}, {})'.format(query, count)
+    return '=HYPERLINK("{}"; {})'.format(query, count)
 
 def get_col_f(website):
     template = 'https://github.com/search?p=8&q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+milestone%3Aneedsdiagnosis'
     query = template.format(website)
     search_template = 'https://api.github.com/search/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+milestone%3Aneedsdiagnosis'
     search = search_template.format(website)
-    response = requests.get(search, headers = {'Authorization': 'token {}'.format(GITHUB_API_TOKEN)}).json()
+    response = api_request(search, headers = {'Authorization': 'token {}'.format(GITHUB_API_TOKEN)}).json()
     count = response['total_count']
-    return '=HYPERLINK({}, {})'.format(query, count)
+    return '=HYPERLINK("{}"; {})'.format(query, count)
 
 def get_col_g(website):
     template = 'https://github.com/webcompat/web-bugs/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+milestone%3Asitewait'
     query = template.format(website)
     search_template = 'https://api.github.com/search/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+milestone%3Asitewait'
     search = search_template.format(website)
-    response = requests.get(search, headers = {'Authorization': 'token {}'.format(GITHUB_API_TOKEN)}).json()
+    response = api_request(search, headers = {'Authorization': 'token {}'.format(GITHUB_API_TOKEN)}).json()
     count = response['total_count']
-    return '=HYPERLINK({}, {})'.format(query, count)
+    return '=HYPERLINK("{}"; {})'.format(query, count)
 
 
 if __name__ == '__main__':
