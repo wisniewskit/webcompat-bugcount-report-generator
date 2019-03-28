0e55160e9adea96331dc2d50e5387659bd850a2c
Mike Taylor <miket@mozilla.com>
2019-03-28 16:29:52 -0500

    Issue #13 - clean up style issues

--- scrape.py
+++ scrape.py
@@ -11,17 +11,22 @@ from requests_html import HTMLSession
 from tablib import Dataset
 
 DATA_PATH = os.path.join(os.getcwd(), 'data/top-sites.csv')
-EXPORT_PATH = os.path.join(os.getcwd(), 'data/export/{}.csv'.format(datetime.now().strftime('%Y%m%d-%H%M%S')))
+EXPORT_PATH = os.path.join(os.getcwd(), 'data/export/{}.csv'.format(
+    datetime.now().strftime('%Y%m%d-%H%M%S')))
 GITHUB_API_TOKEN = os.environ.get('GITHUB_API_TOKEN')
-REQUEST_HEADERS = {'Authorization': 'token {}'.format(GITHUB_API_TOKEN),
+REQUEST_HEADERS = {
+    'Authorization': 'token {}'.format(GITHUB_API_TOKEN),
     'User-Agent': 'mozilla/webcompat-bugcount-report-generator'}
+
 
 def get_websites(dataset):
     websites = dataset.get_col(0)
     return websites
 
+
 s = requests.Session()
 s.headers.update(REQUEST_HEADERS)
+
 
 def api_request(*args, **kwargs):
     try:
@@ -39,52 +44,57 @@ def api_request(*args, **kwargs):
         time.sleep(60)
         return api_request(*args, **kwargs)
 
+
 def get_col_c(website):
     # we want to add the dot back for Bugzilla search
     website = website.replace(' ', '.')
     last_year = datetime.now() - relativedelta(years=1)
-    template = 'https://bugzilla.mozilla.org/buglist.cgi?f1=OP&bug_file_loc_type=allwordssubstr&o3=greaterthan&list_id=14636479&v3={date}&resolution=---&bug_file_loc={site}&query_format=advanced&f3=creation_ts&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&product=Core&product=Fenix&product=Firefox%20for%20Android&product=Firefox%20for%20Echo%20Show&product=Firefox%20for%20FireTV&product=Firefox%20for%20iOS&product=GeckoView&product=Web%20Compatibility'
+    template = 'https://bugzilla.mozilla.org/buglist.cgi?f1=OP&bug_file_loc_type=allwordssubstr&o3=greaterthan&list_id=14636479&v3={date}&resolution=---&bug_file_loc={site}&query_format=advanced&f3=creation_ts&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&product=Core&product=Fenix&product=Firefox%20for%20Android&product=Firefox%20for%20Echo%20Show&product=Firefox%20for%20FireTV&product=Firefox%20for%20iOS&product=GeckoView&product=Web%20Compatibility'  # noqa
     query = template.format(site=website, date=last_year.strftime("%Y-%m-%d"))
     session = HTMLSession()
     r = session.get(query)
     try:
         count_el = r.html.find('span.bz_result_count', first=True)
         count = int(count_el.text.rstrip('bugs found.'))
-    except:
+    except:  # noqa
         count = 0
     return '=HYPERLINK("{}"; {})'.format(query, count)
 
+
 def get_col_d(website):
-    template = 'https://github.com/search?p=8&q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+state%3Aopen&type=Issues'
+    template = 'https://github.com/search?p=8&q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+state%3Aopen&type=Issues'  # noqa
     query = template.format(website)
-    search_template = 'https://api.github.com/search/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+state%3Aopen&type=Issues'
+    search_template = 'https://api.github.com/search/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+state%3Aopen&type=Issues'  # noqa
     search = search_template.format(website)
     response = api_request(search).json()
     count = response['total_count']
     return '=HYPERLINK("{}"; {})'.format(query, count)
+
 
 def get_col_e(website):
-    template = 'https://github.com/webcompat/web-bugs/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+label%3Aseverity-critical+'
+    template = 'https://github.com/webcompat/web-bugs/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+label%3Aseverity-critical+'  # noqa
     query = template.format(website)
-    search_template = 'https://api.github.com/search/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+label%3Aseverity-critical+'
+    search_template = 'https://api.github.com/search/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+label%3Aseverity-critical+'  # noqa
     search = search_template.format(website)
     response = api_request(search).json()
     count = response['total_count']
     return '=HYPERLINK("{}"; {})'.format(query, count)
+
 
 def get_col_f(website):
-    template = 'https://github.com/search?p=8&q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+milestone%3Aneedsdiagnosis'
+    template = 'https://github.com/search?p=8&q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+milestone%3Aneedsdiagnosis'  # noqa
     query = template.format(website)
-    search_template = 'https://api.github.com/search/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+milestone%3Aneedsdiagnosis'
+    search_template = 'https://api.github.com/search/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+milestone%3Aneedsdiagnosis'  # noqa
     search = search_template.format(website)
     response = api_request(search).json()
     count = response['total_count']
     return '=HYPERLINK("{}"; {})'.format(query, count)
 
+
 def get_col_g(website):
-    template = 'https://github.com/webcompat/web-bugs/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+milestone%3Asitewait'
+    template = 'https://github.com/webcompat/web-bugs/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+milestone%3Asitewait'  # noqa
     query = template.format(website)
-    search_template = 'https://api.github.com/search/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+milestone%3Asitewait'
+    search_template = 'https://api.github.com/search/issues?q={}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+milestone%3Asitewait'  # noqa
     search = search_template.format(website)
     response = api_request(search).json()
     count = response['total_count']
